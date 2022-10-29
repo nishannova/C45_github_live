bef8b97a3a776867f4103f3df9ea942d340d6d0e
Nishan Ali <nishanali@Nishans-MacBook-Pro.local>
2022-10-29 19:11:52 +0530

    Addition of Multiplication

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from summation import summation
+from multiplication import multiplication
 
 if __name__=="__main__":
     x = int(input("Enter an integer: "))
     y = int(input("Enter an integer: "))
 
     sum_res = summation(x,y)
-
+    mult_res = multiplication(x,y)
     print(f"Summation of {x} & {y} is: {sum_res}")
+    print(f"Multiplication of {x} & {y} is: {mult_res}")
